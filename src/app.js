7c13f2bbf7e69cef9fc12a2fe3d8b90f8f705ad6
kgsng <keithgeoffrey.ng@stratpoint.com>
2025-05-29 20:31:58 +0800

    adjusted the proper port

--- src/app.js
+++ src/app.js
@@ -27,7 +27,7 @@ const swaggerOptions = {
     },
     servers: [
       {
-        url: `http://localhost:${process.env.PORT || 3000}/api`,
+        url: `http://localhost:${process.env.PORT || 5000}/api`,
       },
     ],
     components: {
